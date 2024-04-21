8a500319087a8b5e55ed787f34da2370b392392d
Diego Suárez Ramos <dsuaran@gmail.com>
2024-04-21 16:30:50 +0200

    camara

--- Assets/camera_script.cs
+++ Assets/camera_script.cs
@@ -1,32 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using Unity.VisualScripting;
+// using UnityEngine;
 
-public class camera_script : MonoBehaviour
-{
-    [SerializeField] Transform pibe;
-    [SerializeField] float distance = 5;
-    [SerializeField] float sensibilidad = 3;
+// public class camera_script : MonoBehaviour
+// {
+//     [SerializeField] Transform pibe;
+//     [SerializeField] float distance = 5;
+//     [SerializeField] float sensibilidad = 3;
 
-    float rY;
-    float rX;
+//     float rY;
+//     float rX;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+//     // Start is called before the first frame update
+//     void Start()
+//     {
 
-    // Update is called once per frame
-    void Update()
-    {
-        rY += Input.GetAxis("Mouse X");
-        rX += Input.GetAxis("Mouse Y");
+//     }
 
-        Quaternion mouseRotation = Quaternion.Euler(-rX*sensibilidad, rY*sensibilidad, 0);
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         rY += Input.GetAxis("Mouse X");
+//         rX += Input.GetAxis("Mouse Y");
 
-        transform.position = pibe.position - mouseRotation*Vector3.forward*distance;
-        transform.rotation = mouseRotation;
-    }
-}
+//         Quaternion mouseRotation = Quaternion.Euler(-rX*sensibilidad, rY*sensibilidad, 0);
+
+//         transform.position = pibe.position - mouseRotation*Vector3.forward*distance;
+//         transform.rotation = mouseRotation;
+//     }
+// }
